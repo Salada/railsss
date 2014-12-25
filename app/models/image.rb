85c7d51491a0242f667160a2a4a19262b0e3c5ff
c4rneade <withixy@gmail.com>
2014-12-25 18:34:18 +0900

    separate gallery partial

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
-  has_attached_file :file
+  has_attached_file :file, styles: { thumb: "100x100" }
   validates_attachment :file, presence: true, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
